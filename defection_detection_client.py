65da9cb560dd20001262f0ba9a00d48c691fa449
sahilbadyal <sbadyals@gmail.com>
2018-05-31 21:40:33 +0530

    Added client

--- defection_detection_client.py
+++ defection_detection_client.py
@@ -1,20 +1,3 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-#!/usr/bin/env python2.7
-
 """Send JPEG image to tensorflow_model_server loaded with inception model.
 """
 
@@ -30,15 +13,16 @@ from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 from models.data_util import getModelHelper
 from models import util
+from models.defs import LBLS
 import numpy as np
 import json
 from pprint import pprint
 import time
+from google.protobuf.json_format import MessageToJson
 
 
 tf.app.flags.DEFINE_string('server', '10.10.17.221:9003',
                            'PredictionService host:port')
-#tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
 FLAGS = tf.app.flags.FLAGS
 
 class Config:
@@ -63,7 +47,7 @@ def main(_):
   
   val_list = None
 
-  with open('./valEqual', "r") as val_listfile:
+  with open('./valEqual.csv', "r") as val_listfile:
           val_list = util.read_csv(val_listfile)
 
   print('Time Started')
@@ -73,22 +57,22 @@ def main(_):
 
   helper = getModelHelper(config)
 
-  
-  data= helper.load_and_preprocess_data(val_list)
-
-  print(np.shape(data)) #, np.shape(label))
-  
-  for dataPoint in data:
+  data = helper.load_and_preprocess_data(val_list)
+  i = 0
+  for dataPoint in data[0]:
           request = predict_pb2.PredictRequest()
           request.model_spec.name = 'predict_defect'
           request.model_spec.signature_name = 'predict_defect'
-          request.inputs['input'].CopyFrom(
-                  tf.contrib.util.make_tensor_proto(data, shape=np.shape(data)) )
+          request.inputs['image'].CopyFrom(
+          tf.contrib.util.make_tensor_proto(dataPoint, shape=np.shape(dataPoint)) )
           result = stub.Predict(request, 100.0)  # 100 secs timeout
-          from google.protobuf.json_format import MessageToJson
           result = MessageToJson(result)
-          print(result)
-          print('Time Taken for 10 requests: ',time.clock() - start)
+          resultD = json.loads(result)
+          floatVal = resultD['outputs']['label']['floatVal']
+          index = np.argmax(floatVal)
+          print ("Predicted %s Actual %s"%(LBLS[index], LBLS[int(val_list[i][1])]))
+          i += 1
+  print('Time Taken for 10 requests: ',time.clock() - start)
   return
 
 
